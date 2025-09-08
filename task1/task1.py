ad2c4554f9eb781921ce34927a71c64aa7576239
cubinCheese <linalbert24@gmail.com>
2025-09-07 23:53:54 -0700

    refined logic of node connections. now figuring out client thread connection logic bug. test2,test3 folders are just for testing.

--- task1/task1.py
+++ task1/task1.py
@@ -1,6 +1,12 @@
 # Assignment says we need to use threading to run both client and server in one process
 # so this combines client.py and server.py that we had before
 
+# this is the message/connection flow: 
+# [client1 --> server2 --> client2 --> server3 --> client3 --> server1 --> client1]
+# clientN & serverN are two parts of the same node N (sharing the same process & uuid)
+# defined in two threads, one functions as receiver, the other as sender.
+
+
 import threading
 import time
 import uuid
@@ -19,29 +25,39 @@ def log_message(message_type, msg, comparison, state, leader_id=None):
     elif message_type == "Leader":
         logging.info(f"Leader is decided to {leader_id}")
     
+# shared state class for client & server thread
+# i.e. shared data like: uuid
+class NodeState:
+    def __init__(self):
+        self.local_node_uuid = uuid.uuid4() # generate uuid for this node
+        self.leader_elected = False   # flag to indicate if leader elected
+
+        self.current_message = None 
 
 class Message:
 
     # message thread is initalized with ID
     def __init__(self, received_uuid=None, flag=0):
         super().__init__()
-        self.received_uuid = received_uuid or str(uuid.uuid4())# received from sender (client)
+        self.received_uuid = received_uuid or uuid.uuid4() # received from sender (client)
         self.flag = flag # flag to indicate if leader elected
 
     # json format
     def msg_to_json(self):
-        return json.dumps(self.__dict__) + "\n"
+        return json.dumps({'received_uuid': str(self.received_uuid), 'flag': self.flag}) + "\n"
     
     # json -> object
     @staticmethod
     def json_to_msg(data):
         msgDict = json.loads(data)
-        return Message(msgDict['received_uuid'], msgDict['flag'])
+        received_uuid = uuid.UUID(msgDict['received_uuid'])
+
+        return Message(received_uuid, msgDict['flag'])
     
 
-    def run(self):
+'''    def run(self):
         print(f"Message from {self.received_uuid} \n")
-        time.sleep(5)
+        time.sleep(5)'''
 
 
 # function to read config.txt for server IP and PORT
@@ -57,70 +73,85 @@ def read_config_file():
 
         return server_ip, int(server_port), client_ip, int(client_port)
     
+# the receiver
+# server() takes server_port, and sharedState object (data class)
+def server(server_ip, server_port, sharedState):
+    print("I am the Server---------- This is my ID:")
 
-def server():
-    print("I am the Server----------")
-    server_ip, serverPort, client_ip, client_port = read_config_file()
-    print(f"Server IP: {server_ip}, Server Port: {serverPort}")
-    print(f"Client IP: {client_ip}, Client Port: {client_port}")
-
-    # generate uuid for this server node
-    node_uuid = uuid.uuid4().hex
-
-    #serverPort = 5002
     serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind(('', serverPort))
-    serverSocket.listen(1)
+    serverSocket.bind(('', server_port))  # port server is using
+    serverSocket.listen(1)                # server is listening
 
     print("The server is ready to receive")
 
     # accepting connection needs to be external to while loop -- otherwise it gets stuck after first request is served
-    connectionSocket, addr = serverSocket.accept()
+    # "if someone connects to the port im listening on, ill accept"
+    connectionSocket, addr = serverSocket.accept() 
 
     while True:
+        # receive message from incoming connection
         sentence = connectionSocket.recv(1024).decode()
         
+        # convert to message object
         message = Message.json_to_msg(data=sentence)
+
         print(f"Received from uuid: {message.received_uuid} with leader elected: {message.flag}")
 
+        # server() should only be responsible for updating the shared NodeState
+        #      based on what message they received.
         # case: leader already elected - would never reach us
-
         # case: we are the leader - UUID has returned back to us
-        if message.received_uuid == node_uuid:
+        if message.received_uuid == sharedState.local_node_uuid:
             # print final message, and close connection
             print("Leader already elected: ", message.received_uuid)
-            log_message("Received", message, "same", "1", node_uuid)
-            connectionSocket.send(Message(message.received_uuid, flag=1).msg_to_json().encode())
+            log_message("Received", message, "same", "1", sharedState.local_node_uuid)
+            #connectionSocket.send(Message(message.received_uuid, flag=1).msg_to_json().encode())
+            
+            # uuid remains same
+            message.flag = 1
             log_message("Sent", message, "", 1)
+
+            # update flags - indicating leader elected
+            sharedState.leader_elected = True
+            sharedState.current_message = message
+
+            # close connection with client
             connectionSocket.close()
             break
         
-        # case: our server uuid < received uuid
+        # case: our node uuid < received uuid
         # just pass message along (we're not the leader)
-        elif message.received_uuid > node_uuid:
+        elif message.received_uuid > sharedState.local_node_uuid:
             print(f"Forwarding message along: {message.received_uuid}")
             
             log_message("Received", message, "greater", "0")
-            connectionSocket.send(message.msg_to_json().encode())
+            #connectionSocket.send(message.msg_to_json().encode())
+            # higher uuid wins, we don't modify message object
             log_message("Sent", message, "", 0)
 
-        # case: our server uuid > received uuid
+        # case: our node uuid > received uuid
         # we are a better candidate for leader, modify message
-        else: # message.received_uuid < node_uuid:
-            print(f"Modifying message to our uuid: {node_uuid}")
+        else: # message.received_uuid < sharedState.local_node_uuid:
+            print(f"Modifying message to our uuid: {sharedState.local_node_uuid}")
 
             log_message("Received", message, "less", "0")
-            connectionSocket.send(Message(node_uuid, flag=0).msg_to_json().encode())
+            #connectionSocket.send(Message(sharedState.local_node_uuid, flag=0).msg_to_json().encode())
+            # update message object with our uuid
+            message.received_uuid = sharedState.local_node_uuid
+            message.flag = 0
+
+            sharedState.current_message = message
+
             log_message("Sent", message, "", 0)
 
-def client():
-    print("I am the Client----------")
-    server_ip, server_port, client_ip, client_port = read_config_file()
-    print(f"Server IP: {server_ip}, Server Port: {server_port}")
-    print(f"Client IP: {client_ip}, Client Port: {client_port}")
+# functions as transmitter for the (client, server) node
+# it passes along whatever the server logic decided was the msg
+def client(client_ip, client_port, sharedState):
 
-    # generate uuid
-    node_uuid = uuid.uuid4().hex
+    print("I am the Client---------- This is my ID:", sharedState.local_node_uuid)
+
+    # pull message from shared state
+    message = sharedState.current_message
 
     # identify socket to connect to
     clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -128,69 +159,58 @@ def client():
     # wait a bit for server to come online
     time.sleep(1)
 
-    # establish connection with server
-    clientSocket.connect((server_ip, server_port))
+    # establish connection with external server (external node)
+    clientSocket.connect((client_ip, client_port))
 
     while True:
-            
-        # create message obj with our own uuid
-        message = Message(received_uuid=node_uuid)
 
-        log_message("Sent", message, "", 0)
+        log_message("Sent", message, "", message.flag)
 
+        # client sends a message to external server
         clientSocket.send(message.msg_to_json().encode())
         print(f"Sent message with uuid: {message.received_uuid} and flag: {message.flag}")
 
-        # server response
-        response = clientSocket.recv(1024)
-        print("From Server: ", response.decode())
-        if response:
-            msg = Message.json_to_msg(response)
-            print(f"Received message with uuid: {msg.received_uuid}, flag: {msg.flag}")
+        # client expects external server response -- asynch ring doesnt want this
+        # response = clientSocket.recv(1024)
 
-            # Log received message
-            if msg.flag == 1:
-                log_message("Received", msg, "same", "1", msg.received_uuid)
-                print(f"Leader is {msg.received_uuid}")
-                log_message("Leader", msg, "", "", msg.received_uuid)
-                break  # Exit the loop when leader is elected
+        print(f"Received message with uuid: {message.received_uuid}, flag: {message.flag}")
 
-            # Otherwise, forward the message
-            time.sleep(1)
+        # Log received message
+        # now we evaluate the message we sent out (do we need to close connection?)
+        # leader was elected already (told by our own local server thread)
+        if message.flag == 1:
+            log_message("Received", message, "same", "1", message.received_uuid)
+            print(f"Leader is {message.received_uuid}")
+            log_message("Leader", message, "", "", message.received_uuid)
+            break  # Exit the loop when leader is elected
+        #else:
+        #    continue looping
 
     clientSocket.close()
 
 
 def main():
+
+    # server() only receives message and updates shared message state
+    # client() only sends message based on shared message state
+    
+    server_ip, server_port, client_ip, client_port = read_config_file()
+    print(f"Server IP: {server_ip}, Server Port: {server_port}") # this is us
+    print(f"Client IP: {client_ip}, Client Port: {client_port}") # this is external node
+
+    sharedState = NodeState()
+
     # Start server and client threads for Node 1, Node 2, and Node 3
-    server_thread_1 = threading.Thread(target=server)
-    client_thread_1 = threading.Thread(target=client)
-
-    server_thread_2 = threading.Thread(target=server)
-    client_thread_2 = threading.Thread(target=client)
-
-    server_thread_3 = threading.Thread(target=server)
-    client_thread_3 = threading.Thread(target=client)
+    server_thread = threading.Thread(target=server, args=(server_ip, server_port, sharedState))
+    client_thread = threading.Thread(target=client, args=(client_ip, client_port, sharedState))
 
     # Start the threads
-    server_thread_1.start()
-    client_thread_1.start()
-
-    server_thread_2.start()
-    client_thread_2.start()
-
-    server_thread_3.start()
-    client_thread_3.start()
+    server_thread.start()
+    client_thread.start()
 
     # Wait for all threads to finish
-    server_thread_1.join()
-    client_thread_1.join()
-
-    server_thread_2.join()
-    client_thread_2.join()
-
-    server_thread_3.join()
-    client_thread_3.join()
+    server_thread.join()
+    client_thread.join()
 
 
 if __name__ == "__main__":
